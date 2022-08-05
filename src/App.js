95da6707ad0dda45740842d46378fb018f29eb3b
serhanmiah <serhanmiah@icloud.com>
2022-08-05 13:07:28 +0100

    MVP with styles

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { useState, useEffect, createContext } from "react";
+import Question from "./components/Question";
+import axios from "axios";
+import { Button } from "react-bootstrap";
+
+import PageNavBar from "./components/PageNavBar";
+// ! react BootStrap 
+import About from './components/Home/About'
+
+
+import logo from './Img/attachment_95890421-removebg-preview.png'
+
+function App() {
+
+  const [testData, setTestData] = useState([])
+
+
+  useEffect(() => {
+
+      const getData = async () => {
+        try {
+          const { data } = await axios.get('https://the-trivia-api.com/api/questions')
+          // console.log(data)
+          setTestData(data)
+          // ! this will be the entire API library
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      getData()
+    }, [])
+  
+
+  const [visibleQuestion, setVisible] = useState(false)
+
+  const [startVisibility, setStartVisibility] = useState(true)
+
+  const questionVisibility = () => { 
+    setVisible(true) 
+    setStartVisibility(false)
+  }
+
+  return (
+    <div className="App">
+      
+        <PageNavBar id="basic-navbar-nav" className='justify-content-end' />
+
+        {/* <About /> */}
+        {/* Make it clickable event with useState visibility */}
+        <div className="hero">
+        {/* <PageNavbar /> */}
+        <div className="image-logo">
+          <img src={logo} alt='logo'   />
+          {/* just a small image maybe need to make it small */}
+        </div>
+        {/* <h1>Welcome To The Trivia Quiz!</h1> */}
+        
+        {/* Need to hide H3 and button once it is clicked */}
+        
+        {startVisibility ? <>
+          <h6 className="font-weight-bold text-center display-6">Click on the Start button to begin the questions!</h6>
+          <div className="d-grid gap-2">
+          <button className="btn btn-success btn-lg"  onClick={questionVisibility}><h4>Start Quiz</h4></button>
+        </div>
+        </> : ""
+        } 
+        {visibleQuestion && <Question testData={testData} setStartVisibility={setStartVisibility} setVisible={setVisible} onClick={questionVisibility} />}
+        </div>
+    
+      <footer className="footer-wrapper">
+      <div className="text-center text-white p-3 " variant='dark' bg="dark" >
+            Made By Alex & Serhan
+            {/* variant="dark" */}
+    
+      </div>
+ 
+    </footer>
+    </div>
+    
+    
+  )
+}
+
+export default App;
+
+
+
---
+++ src/components/Question.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import { useState, createContext } from 'react'
+import { Button, Col, Container } from 'react-bootstrap'
+
+
+
+// const [ currentQuestion, setCurrentQuestion ] = useState('')
+
+// let currentIndex = 0
+
+const Question = ({ testData, setStartVisibility, setVisible }) => {
+
+  function showStart() {
+    setStartVisibility(true)
+    setVisible(false)
+  }
+
+  const [questionIndex, setQuestionIndex] = useState(0)
+
+  const [score, setScore] = useState(0)
+
+  function compareAnswers(event, correctAnswer) {
+    setQuestionIndex(questionIndex + 1)
+
+    if (event.target.value === correctAnswer) {
+      setScore(score + 1)
+    }
+  }
+ 
+  let newArray
+
+  function arrayMagic(correctAnswer, incorrectAnswers) {
+    newArray = [...incorrectAnswers, correctAnswer ].sort()
+  }
+
+
+
+  return (
+    <>
+      <div className='hero-container'>
+        {testData.map((item, index) => {
+
+          const { question, correctAnswer, incorrectAnswers, category, difficulty } = item
+          
+          if (index === questionIndex) {
+            {/* {createArray(correctAnswer, incorrectAnswers)} */ }
+            { arrayMagic(correctAnswer, incorrectAnswers) }
+            return (
+              <div className='question-section'>
+                <div className='text'>
+                <h3>Question category: {category}</h3>
+                <h3>Question difficulty: {difficulty}</h3>
+                <h3>Question {index + 1} out of {testData.length} </h3>
+                <h2>Your score is: {score}</h2>
+                <h3>{question}</h3>
+                </div>
+                {newArray.map(answer => {
+                  return (
+                  <div className='button-correct'>
+                  <Col  className='w-100 d-none d-md-block mb-3'>
+                  <Button  className='btn btn-lg btn-block' variant="outline-primary" size="lg" value={answer} onClick={(event) => { compareAnswers(event, correctAnswer); }}>{answer}</Button>
+                  </Col>
+                 </div>
+                  )
+                  })}
+                
+              </div>
+            )
+          }
+        })}
+
+        {questionIndex >= testData.length ? [<h1 className='display-3'>Quiz complete! Your final score is: {score}</h1>, <Button className='newQuiz' variant="primary" size="lg" onClick={showStart}>
+          <h2 className='start-newQuiz'>Start new quiz</h2>
+        
+          </Button>, testData.map((item, index) => {
+          const { question, correctAnswer, incorrectAnswers } = item
+          return(
+  
+              <Container as='main'>
+                <div className='questionAnswered'>
+                <h3 className='question'>{question}</h3>
+                <h3 className='answers'>Correct answer: {correctAnswer}</h3>
+                <h3 className='incorrect-answer'>Incorrect answers: {incorrectAnswers.map((answer, index) => {
+                  return (index + 1 === incorrectAnswers.length ? `${answer}`: `${answer}, `)
+                })}</h3>
+
+                </div>
+            
+              </Container>
+           
+          )
+        })  ]: ""  } 
+
+        </div>
+
+
+
+    </>
+  )
+}
+
+export default Question
